8e337e18b89a745e92bcaf3af3ce0b55c004f605
Michael Klein <mbklein@gmail.com>
2011-08-04 15:23:28 -0700

    Version bump/README update

--- lib/checksum-tools.rb
+++ lib/checksum-tools.rb
@@ -1,6 +1,6 @@
 module Checksum
   module Tools
-    VERSION = "0.9.3"
+    VERSION = "0.9.4"
     
     CHUNK_SIZE = 1048576 # 1M blocks
     DEFAULT_OPTS = { :overwrite => false, :recursive => false, :exclude => [], :extension => '.digest' }
