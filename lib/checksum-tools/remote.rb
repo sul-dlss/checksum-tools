sul-dlss/checksum-tools

commit 14c0bdf0dbf91aec912f855df99e0c1ec04e6f52

Ensure Checksum::Tools::Remote#exec! always uses bash on the remote end

lib/checksum-tools/remote.rb
@@ -44,7 +44,15 @@ module Checksum::Tools
     end
 
     def exec!(cmd)
-      ssh.exec!(cmd).chomp
+      result = ''
+      ssh do |ch|
+        ch.exec("bash -l") do |ch2, success|
+          ch2.on_data { |c,data| result += data }
+          ch2.send_data "#{cmd}\n"
+          ch2.send_data "exit\n"
+        end
+      end
+      result.chomp
     end
     
     def digests

spec/checksum-tools/remote_spec.rb
@@ -62,6 +62,10 @@ describe Checksum::Tools::Remote do
       @tool.stub!(:remote_properties).and_return({ :openssl => 'openssl' })
       @tool.stub!(:sftp).and_return(sftp)
       @tool.stub!(:ssh).and_return(ssh)
+      # KLUDGE ALERT: We can't mock an entire asynchronous SSH session, so we're 
+      #               stubbing Checksum::Tools::Remote#exec! which is one of the 
+      #               methods we should be testing!
+      @tool.stub!(:exec!) { |c| %x[#{c} 2>/dev/null].chomp }
     end
   
     after :each do

commit 5945c2a0c8980c20a9b49fc3e10b382c54488109

Fix bug in retrieval of openssl-supported digest types from remote (introduced in 14c0bdf0dbf91aec912f855df99e0c1ec04e6f52)

lib/checksum-tools/remote.rb
@@ -57,7 +57,7 @@ module Checksum::Tools
     
     def digests
       resp = ''
-      resp = exec! "#{openssl} dgst -h"
+      resp = exec! "#{openssl} dgst -h 2>&1"
       resp.scan(/-(.+?)\s+to use the .+ message digest algorithm/).flatten.collect { |d| d.to_sym }
     end
   