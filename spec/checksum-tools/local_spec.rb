c37376e8b020ff865790e3ddae37576cc6715e0f
Justin Coyne <jcoyne@justincoyne.com>
2018-04-30 11:00:35 -0500

    Remove rspec deprecations

--- spec/checksum-tools/local_spec.rb
+++ spec/checksum-tools/local_spec.rb
@@ -2,25 +2,25 @@ require File.expand_path('../../spec_helper', __FILE__)
 require 'tempfile'
 
 describe Checksum::Tools::Local do
-  
+
   describe "class methods" do
-     
+
      it "should return false when registering an existing digest type" do
-       (Checksum::Tools::Local.register_digest(:md5,'digest/md5') { Digest::MD5.new }).should be_false
+       (Checksum::Tools::Local.register_digest(:md5,'digest/md5') { Digest::MD5.new }).should be false
      end
-     
+
      it "should return false when a digest class fails to load" do
-       (Checksum::Tools::Local.register_digest(:sha999,'digest/sha999') { Digest::MD5.new }).should be_false
+       (Checksum::Tools::Local.register_digest(:sha999,'digest/sha999') { Digest::MD5.new }).should be false
      end
-     
+
      it "should return a digest handler" do
        Checksum::Tools::Local.digest_for(:md5).should be_a(Digest::MD5)
      end
-     
+
      it "should raise an error when an unsupported digest type is requested" do
        lambda { Checksum::Tools::Local.digest_for(:sha999) }.should raise_error(ArgumentError)
      end
-     
+
   end
 
   describe "instance methods" do
@@ -40,23 +40,23 @@ describe Checksum::Tools::Local do
       @tool.digest_length(:md5).should == 32
       @tool.digest_length(:sha1).should == 40
     end
-    
+
     it "should generate digests for an IO stream" do
       @listener.should_receive(:progress).once.with(26)
       result = @tool.digest_stream(@io) { |*args| @listener.progress(*args) }
       result.should == @digests
     end
-    
+
     it "should verify digests for an IO stream" do
       @listener.should_receive(:progress).once.with(26)
       result = @tool.verify_stream(@io, @digests) { |*args| @listener.progress(*args) }
       result.should == { :md5 => true, :sha1 => true }
     end
-    
+
   end
-  
+
   describe "file operations" do
-    
+
     before :all do
       @tmpdir = Dir.tmpdir
       tarfile = File.expand_path('../../test_data.tgz',__FILE__)
@@ -71,13 +71,13 @@ describe Checksum::Tools::Local do
     before :each do
       @tool = Checksum::Tools.new({:dir => @dir}, :md5, :sha1, :recursive => true)
     end
-  
+
     it "should calculate checksums for a file" do
       result = @tool.digest_file(File.join(@dir,'one/two/ignore.doc'))
       result.should == { :md5 => 'a8b0d13fd645acc29b0dc2c4837e6f00', :sha1 => '8e129fe06c0679ce5a7f6e58d60cfd488512913a' }
       lambda { @tool.digest_file(File.join(@dir,'one/two/nonexistent.txt')) }.should raise_error(Errno::ENOENT)
     end
-  
+
     it "should generate checksums for a tree of files" do
       listener = double('listener')
       listener.should_receive(:progress).once.with(File.join(@dir,'one/two/report.pdf'),-1,-1)
@@ -90,14 +90,14 @@ describe Checksum::Tools::Local do
       strings = File.read(File.join(@dir,'one/two/report.pdf.digest')).split(/\n/)
       strings.should include("MD5(report.pdf)= fda5eab2335987f56d7d3abe53734295")
       strings.should include("SHA1(report.pdf)= 56ea04102e0388de9b9c31c6db8aebbff67671c1")
-    
+
       strings = File.read(File.join(@dir,'three/video.mp4.digest')).split(/\n/)
       strings.should include("MD5(video.mp4)= 9023e975b52be97a4ef6ad4e25e2ef79")
       strings.should include("SHA1(video.mp4)= ce828086b63e6b351d9fb6d6bc2b0838725bdf37")
-    
-      File.exists?(File.join(@dir,'one/two/ignore.doc.digest')).should be_false
+
+      File.exists?(File.join(@dir,'one/two/ignore.doc.digest')).should be false
     end
-  
+
     it "should pass verification for a tree of files" do
       listener = double('listener')
       listener.should_receive(:progress).once.with(File.join(@dir,'one/two/report.pdf'),-1,-1)
@@ -126,7 +126,7 @@ describe Checksum::Tools::Local do
          |*args| listener.progress(*args)
       end
     end
-  
+
   end
-  
+
 end
--- spec/checksum-tools/remote_spec.rb
+++ spec/checksum-tools/remote_spec.rb
@@ -4,13 +4,13 @@ require 'tempfile'
 describe Checksum::Tools::Remote do
 
   describe "file operations" do
-    
-    before :all do      
+
+    before :all do
       @mock_sftp = Class.new do
         def dir
           Class.new do
             def self.[](*args)
-              Dir[File.join(*args)].collect { |s| 
+              Dir[File.join(*args)].collect { |s|
                 name = s[args.first.length..-1]
                 stat = File.stat(s)
                 type = stat.mode & 0040000 != 0 ? Net::SFTP::Protocol::V04::Attributes::T_DIRECTORY : Net::SFTP::Protocol::V04::Attributes::T_REGULAR
@@ -62,37 +62,37 @@ describe Checksum::Tools::Remote do
       @tool.stub(:remote_properties).and_return({ :openssl => 'openssl' })
       @tool.stub(:sftp).and_return(sftp)
       @tool.stub(:ssh).and_return(ssh)
-      # KLUDGE ALERT: We can't mock an entire asynchronous SSH session, so we're 
-      #               stubbing Checksum::Tools::Remote#exec! which is one of the 
+      # KLUDGE ALERT: We can't mock an entire asynchronous SSH session, so we're
+      #               stubbing Checksum::Tools::Remote#exec! which is one of the
       #               methods we should be testing!
       @tool.stub(:exec!) { |c| %x[#{c} 2>/dev/null].chomp }
     end
-  
+
     after :each do
       @tool = nil
     end
-    
+
     it "should be of the correct type" do
       @tool.should be_a(Checksum::Tools::Remote)
     end
-    
+
     it "should report correct digest lengths" do
       @tool.digest_length(:md5).should == 32
       @tool.digest_length(:sha1).should == 40
     end
-    
+
     it "should report on file size" do
       @tool.send(:file_size,File.join(@dir,'one/two/report.pdf')).should == 134833
       lambda { @tool.send(:file_size,File.join(@dir,'one/two/nonexistent.txt')) }.should raise_error(Errno::ENOENT)
     end
-    
+
     it "should calculate checksums for a file" do
       pending('Checksum::Tools::Remote#exec! is stubbed and does not execute under CI') if ENV['TRAVIS']
       result = @tool.digest_file(File.join(@dir,'one/two/ignore.doc'))
       result.should == { :md5 => 'a8b0d13fd645acc29b0dc2c4837e6f00', :sha1 => '8e129fe06c0679ce5a7f6e58d60cfd488512913a' }
       lambda { @tool.digest_file(File.join(@dir,'one/two/nonexistent.txt')) }.should raise_error(Errno::ENOENT)
     end
-  
+
     it "should generate checksums for a tree of files" do
       pending('Checksum::Tools::Remote#exec! is stubbed and does not execute under CI') if ENV['TRAVIS']
       listener = double('listener')
@@ -103,14 +103,14 @@ describe Checksum::Tools::Remote do
       strings = File.read(File.join(@dir,'one/two/report.pdf.digest')).split(/\n/)
       strings.should include("MD5(report.pdf)= fda5eab2335987f56d7d3abe53734295")
       strings.should include("SHA1(report.pdf)= 56ea04102e0388de9b9c31c6db8aebbff67671c1")
-    
+
       strings = File.read(File.join(@dir,'three/video.mp4.digest')).split(/\n/)
       strings.should include("MD5(video.mp4)= 9023e975b52be97a4ef6ad4e25e2ef79")
       strings.should include("SHA1(video.mp4)= ce828086b63e6b351d9fb6d6bc2b0838725bdf37")
-    
-      File.exists?(File.join(@dir,'one/two/ignore.doc.digest')).should be_false
+
+      File.exists?(File.join(@dir,'one/two/ignore.doc.digest')).should be false
     end
-  
+
     it "should pass verification for a tree of files" do
       pending('Checksum::Tools::Remote#exec! is stubbed and does not execute under CI') if ENV['TRAVIS']
       listener = double('listener')
@@ -141,7 +141,7 @@ describe Checksum::Tools::Remote do
          |*args| listener.progress(*args)
       end
     end
-  
+
   end
-  
+
 end
